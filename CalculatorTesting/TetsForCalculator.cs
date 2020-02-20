74bccbffba2b0c7dbc6ccf52be462f86789e02f0
Konstantin Prokhorov <fandom170@gmail.com>
2020-02-20 09:11:39 +0200

    Updated examples

--- CalculatorTesting/TetsForCalculator.cs
+++ CalculatorTesting/TetsForCalculator.cs
@@ -14,8 +14,11 @@ namespace CalculatorTesting
         double a = 6.6;
         double b = 1.4;
 
-        [Test(ExpectedResult = 8)]
-        public void TestAdding1(double a, double b) 
+        [Test, Combinatorial]
+        public void TestAdding1(
+            [Values(6.6, 1.4, -3.0)] double a,
+            [Values (5.31, -9.14, 0.0)] double b
+            ) 
         {
             calculator.Add(a, b);
         }
@@ -25,6 +28,18 @@ namespace CalculatorTesting
         public void TestAdding2(double a, double b)
         {
             calculator.Add(a, b);
+        }
+
+        [Test(ExpectedResult = 8)]
+        public double TestAdding3()
+        {
+            return (calculator.Add(6.6, 1.4));
+        }
+
+        [Test(Description ="Check of values adding")]
+        public void TestAdding4(double a, double b)
+        {
+            Assert.That(calculator.Add(a, b) == 8.0);
         }
 
     }
